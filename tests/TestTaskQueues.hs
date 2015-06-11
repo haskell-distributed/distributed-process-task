1e50e843c54b248558f7eb90ed4d15f2484845ab
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-06-11 20:24:22 +0300

    Use random port in tests.

--- tests/TestTaskQueues.hs
+++ tests/TestTaskQueues.hs
@@ -141,6 +141,6 @@ main = testMain $ tests
 testMain :: (NT.Transport -> IO [Test]) -> IO ()
 testMain builder = do
   Right (transport, _) <- createTransportExposeInternals
-                                    "127.0.0.1" "10501" defaultTCPParameters
+                                    "127.0.0.1" "0" defaultTCPParameters
   testData <- builder transport
   defaultMain testData
