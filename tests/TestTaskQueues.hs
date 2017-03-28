3bb2530d9e4b296f0b46e7c2ca75b0df88b0b974
Tim Watson <watson.timothy@gmail.com>
2017-03-28 22:49:29 +0100

    Adjust for upstream changes

--- tests/TestTaskQueues.hs
+++ tests/TestTaskQueues.hs
@@ -5,23 +5,27 @@
 
 module Main where
 
-import Control.Distributed.Process hiding (call)
-import Control.Distributed.Process.Closure
-import Control.Distributed.Process.Node
+import Control.Distributed.Process hiding (call, catch)
 import Control.Distributed.Process.Async
-import Control.Distributed.Process.ManagedProcess
+import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Extras.Time
 import Control.Distributed.Process.Extras.Timer
 import Control.Distributed.Process.Extras.Internal.Types
+import Control.Distributed.Process.Node
+import Control.Distributed.Process.Management
+  ( MxAgentId(..)
+  , MxEvent(MxRegistered)
+  , mxAgent
+  , mxSink
+  , mxReady
+  , liftMX
+  )
+import Control.Distributed.Process.ManagedProcess
 import Control.Distributed.Process.Serializable()
-import Control.Distributed.Process.Tests.Internal.Utils
+import Control.Distributed.Process.SysTest.Utils
 
 import Control.Distributed.Process.Task.Queue.BlockingQueue hiding (start)
 import qualified Control.Distributed.Process.Task.Queue.BlockingQueue as Pool (start)
-
-#if ! MIN_VERSION_base(4,6,0)
-import Prelude hiding (catch)
-#endif
 
 import Test.Framework (Test, defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
@@ -47,6 +51,11 @@ crashingTask sp = getSelfPid >>= sendChan sp >> die "Boom"
 
 $(remotable ['sampleTask, 'namedTask, 'crashingTask])
 
+waitForDown :: MonitorRef -> Process DiedReason
+waitForDown ref =
+  receiveWait [ matchIf (\(ProcessMonitorNotification ref' _ _) -> ref == ref')
+                        (\(ProcessMonitorNotification _ _ dr) -> return dr) ]
+
 -- SimplePool tests
 
 startPool :: SizeLimit -> Process ProcessId
@@ -58,42 +67,64 @@ testSimplePoolJobBlocksCaller :: TestResult (AsyncResult (Either ExitReason Stri
 testSimplePoolJobBlocksCaller result = do
   pid <- startPool 1
   -- we do a non-blocking test first
-  job <- return $ ($(mkClosure 'sampleTask) (seconds 2, "foobar"))
-  callAsync pid job >>= wait >>= stash result
+  job <- return $ ($(mkClosure 'sampleTask) (seconds 2, "foobar")) :: Process (Closure (Process String))
+  call pid job >>= stash result . AsyncDone
 
 testJobQueueSizeLimiting ::
-    TestResult (Maybe (AsyncResult (Either ExitReason String)),
-                Maybe (AsyncResult (Either ExitReason String)))
+    TestResult ((AsyncResult (Either ExitReason String)),
+                (AsyncResult (Either ExitReason String)))
                          -> Process ()
 testJobQueueSizeLimiting result = do
+  (sp1, rp1) <- newChan
+  (sp2, rp2) <- newChan
+  listener <- mxAgent (MxAgentId "test.listener") () [
+                        (mxSink $ \(ev :: MxEvent) -> do
+                          case ev of
+                            MxRegistered _ name -> do
+                              case name of
+                                "job1" -> liftMX (sendChan sp1 ()) >> mxReady
+                                "job2" -> liftMX (sendChan sp2 ()) >> mxReady
+                                _      -> mxReady
+                            _                 -> mxReady)
+                          ]
+
   pid <- startPool 1
   job1 <- return $ ($(mkClosure 'namedTask) ("job1", "foo"))
   job2 <- return $ ($(mkClosure 'namedTask) ("job2", "bar"))
   h1 <- callAsync pid job1 :: Process (Async (Either ExitReason String))
+  m1 <- monitorAsync h1
+
+  -- we can get here *very* fast, so give the registration time to kick in
+  () <- receiveChan rp1
+  Just p <- whereis "job1"
+
   h2 <- callAsync pid job2 :: Process (Async (Either ExitReason String))
+  m2 <- monitorAsync h2
 
   -- despite the fact that we tell job2 to proceed first,
   -- the size limit (of 1) will ensure that only job1 can
   -- proceed successfully!
-  nsend "job2" ()
   AsyncPending <- poll h2
   Nothing <- whereis "job2"
 
-  -- we can get here *very* fast, so give the registration time to kick in
-  sleep $ milliSeconds 250
-  j1p <- whereis "job1"
-  case j1p of
-    Nothing -> die $ "timing is out - job1 isn't registered yet"
-    Just p  -> send p ()
+  send p ()
+  waitForDown m1
 
   -- once job1 completes, we *should* be able to proceed with job2
   -- but we allow a little time for things to catch up
-  sleep $ milliSeconds 250
-  nsend "job2" ()
+  () <- receiveChan rp2
+  Just p2 <- whereis "job2"
+  send p2 ()
 
-  r2 <- waitTimeout (within 2 Seconds) h2
-  r1 <- waitTimeout (within 2 Seconds) h1
-  stash result (r1, r2)
+  waitForDown m2
+
+  monitor p >>= waitForDown
+  monitor p2 >>= waitForDown
+
+  r2' <- wait h2
+  r1' <- wait h1
+  stash result (r1', r2')
+  kill listener "done"
 
 testExecutionErrors :: TestResult Bool -> Process ()
 testExecutionErrors result = do
@@ -125,8 +156,8 @@ tests transport = do
          (delayedAssertion
           "expected the server to block the second job until the first was released"
           localNode
-          (Just (AsyncDone (Right "foo")),
-           Just (AsyncDone (Right "bar"))) testJobQueueSizeLimiting)
+          ((AsyncDone (Right "foo")),
+           (AsyncDone (Right "bar"))) testJobQueueSizeLimiting)
        , testCase "Crashing Tasks are Reported Properly"
          (delayedAssertion
           "expected the server to report an error"
